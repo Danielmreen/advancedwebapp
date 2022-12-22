6d6bab34ef0340e5d719244127a76316162b2f40
Danielmreen <danielamreen@gmail.com>
2022-12-22 22:04:39 +0800

    init

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,13 @@ Route::get('/', function () {
 });
 
 Route::get('/home',[homeControl::class,"index"]);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
