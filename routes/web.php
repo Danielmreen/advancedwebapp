07d6f09119a725a73fbc64e0a1d547a55d357ec0
Danielmreen <danielamreen@gmail.com>
2022-12-22 22:02:26 +0800

    init

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\homeControl;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,7 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/',function()
-{
-  
-});
+Route::get('/home',[homeControl::class,"index"]);
