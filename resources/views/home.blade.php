5ee1c6963df534b9f760ea9458d003845b3a49aa
Danielmreen <danielamreen@gmail.com>
2022-12-22 22:31:58 +0800

    init

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -59,9 +59,23 @@ https://templatemo.com/tm-580-woox-travel
                     <ul class="nav">
                         <li><a href="index.html" class="active">Home</a></li>
                         <li><a href="about.html">About</a></li>
-                        <li><a href="deals.html">Deals</a></li>
-                        <li><a href="reservation.html">Reservation</a></li>
-                        <li><a href="reservation.html">Book Yours</a></li>
+                        <li><a href="deals.html">Explore</a></li>
+                        <li>@if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <li>
+                          <x-app-layout>
+                         </x-app-layout>
+                        </li>
+                    @else
+                    <li><a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
+
+                        @if (Route::has('register'))
+                        <li><a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a></li>
+                        @endif
+                    @endauth
+                </div>
+            @endif</li>
                     </ul>   
                     <a class='menu-trigger'>
                         <span>Menu</span>
